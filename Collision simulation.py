1ca0196b60763b59c4d1d3705426e7e16e040027
rotemyoeli <42690752+rotemyoeli@users.noreply.github.com>
2019-01-16 21:49:52 +0200

    Add files via upload

--- Collision simulation.py
+++ Collision simulation.py
@@ -1,33 +1,18 @@
 import numpy as np
 import heapq
 import matplotlib.pyplot as plt
+import matplotlib.ticker as plticker
+import csv
 import ast
-import math
-import pandas as pd
-def parse_tuple(string):
-    try:
-        s = ast.literal_eval(str(string))
-        if type(s) == tuple:
-            return s
-        return
-    except:
-        return
-
-def convert_pathes_to_routes(df_pathes):
-    optimal_routes =[]
-    #Iterate over each column
-    for col_idx in range(df_pathes.shape[1]):
-        route_list = list()
-        for row_idx in range(df_pathes.shape[0]):
-            if isinstance(parse_tuple(df_pathes.values[row_idx,col_idx]),tuple):
-                route_list.append(parse_tuple(df_pathes.values[row_idx,col_idx]))
-        optimal_routes.append(route_list)
-    return optimal_routes
-
+import sys
 
 route = []
 new_route = []
-k = 2
+tmp_route = []
+k = 1
+
+org_total_g = 0
+new_total_g = 0
 
 # load the map file into numpy array
 with open('map_data.txt', 'rt') as infile:
@@ -44,23 +29,32 @@ grid = np.array(grid1.astype(np.int))
 fig, ax = plt.subplots(figsize=(10, 5))
 ax.imshow(grid, cmap=plt.cm.ocean)
 
+# csv file name
+filename = "results2.csv"
 
+# reading csv file
+with open(filename, 'rt') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
+    # extracting each data row one by one
+    for row in csvreader:
+        tmp_route = []
+        for y in range(0, len(row)):
+            if row[y]:
+                tmp_route.append(ast.literal_eval(row[y]))
+        route.append(tmp_route)
 
-#route = [[((10, 10), 0), ((10, 11), 1), ((10, 12), 2), ((10, 13), 3), ((10, 14), 4), ((10, 15), 5), ((10, 16), 6), ((10, 17), 7)],
-#         [((11, 10), 0), ((11, 11), 1), ((11, 12), 2), ((11, 13), 3), ((11, 14), 4), ((11, 15), 5), ((11, 16), 6), ((11, 17), 7)],
-#         [((12, 10), 0), ((12, 11), 1), ((12, 12), 2), ((12, 13), 3), ((12, 14), 4), ((12, 15), 5), ((12, 16), 6), ((12, 17), 7)]]
-
-df_optimal_pathes = pd.read_csv("Results.csv")
-route = convert_pathes_to_routes(df_optimal_pathes)
-
-
+for x in range(0, len(route)):
+    org_total_g = org_total_g + len(route[x])
 ##############################################################################
 # heuristic function for path scoring
 ##############################################################################
 
 def heuristic(a, b):
+    #if a[0] is not b[0] and a[1] is not b[1]:
     return (b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2
-
+    #else:
+        #return 0
 
 ##############################################################################
 # path finding function
@@ -68,7 +62,7 @@ def heuristic(a, b):
 
 def astar(array, start, goal, y):
     neighbors = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-    print("Re-calculation A-star algorithm")
+
     start_node = (start)
 
     close_set = set()
@@ -107,7 +101,7 @@ def astar(array, start, goal, y):
             tentative_g_score = gscore[current] + heuristic(current[0], neighbor[0])
 
             ##Check for collision
-            for a in range(0, y):
+            for a in range(0, y): #[k for k in range(0, len(route)) if k != y]:
                 if len(route[a]) > neighbor[1]:
                     if neighbor[0] == route[a][neighbor[1]][0]:
                         is_collision = 1
@@ -145,13 +139,12 @@ def search_for_interrupt_plan():
     open = list()
     pos = list()
     step = 0
-    global k
 
     #Set starting positions for all agents into a node
     for x in range(0, len(route)):
         pos.append(route[x][0][0])
 
-    start = (pos, k, step)
+    start = (pos, k, step, None)
     open.append(start)
     goals = list()
 
@@ -167,116 +160,140 @@ def search_for_interrupt_plan():
             else:
                 open.append(new_node)
 
+    # After all goals have been found
+    max_step = 0
+    best_goal = None
+    for goal in goals:
+        if goal[2] > max_step:
+            max_step = goal[2]
+            best_goal = goal
+
+    # Reconstruct the solution
+    solution = [best_goal]
+    parent = best_goal[3]
+    while parent is not None:
+        solution.append(parent)
+        parent = parent[3]
+        solution = list(reversed(solution))
+    return (solution)
+    # Return the best_goal, it is the one that maximizes the makespan
+
 def is_goal(self):
-    global k
-    if k == 0:
+    if self[1] == 0:
         return True
     for x in range(1, len(self[0])):
         if self[0][x] is not route[x][-1][0]:
             return False
     return True
-    # TODO: IS K=0 OR ALL AT AGOAL
-
 
 def apply(action, node, step):
-    global k
     tmp_node = list()
     pos = list()
 
     if step < len(route[0]):
-        #If action of Agent 0 is according the plan - don't decrease k
+        #If action of Agent 0 is according the plan - don't decrease the no. of remained error
         if route[0][step][0] == (node[0][0][0] + action[0], node[0][0][1] + action[1]):
-            for x in range(0, len(route)):
-                print(str(x)+" "+ str(step)+" ")
-                pos.append(route[x][step][0])
-            tmp_node = (pos, k, step)
+            pos.append(route[0][step][0])
+            for w in range(1, len(route)):
+                if step < len(route[w]):
+                    pos.append(route[w][step][0])
+                else:
+                    pos.append(route[w][-1][0])
+            tmp_node = (pos, k, step, node)
             return tmp_node
+
+        #If action of Agent 0 is not according the plan - decrease the no. of remained error
         else:
             tmp_node = calc_new_routes(action, node, step)
-    else:
-        tmp_node = calc_new_routes(action, node, step)
+
+    #check for collision and if there is:
+
+    # stay at position
+
     return tmp_node
 
 def calc_new_routes(action, node, step):
-    global k
     tmp_node = list()
     pos = list()
-    tmp_node.append((node[0][0][0] + action[0], node[0][0][1] + action[1]))
-    agent_that_interrupt = 0
+    # set the no. of agent that making the move for interruptions
+    a = 0
 
-    #check interrupt for all agent with agent0
+    tmp_node.append((node[0][0][0] + action[0], node[0][0][1] + action[1]))
+
+    #If there are no more errors left then return...
+    if node[1] == 0:
+        for x in range(0, len(route)):
+            if step < len(route[x]):
+                pos.append(route[x][step][0])
+            else:
+                pos.append(route[x][-1][0])
+        tmp_node = (pos, 0, step, node)
+
+        return tmp_node
+
+
+    # Send Agent-0 to A* (grid, start = next new step, goal, agent no.)
+    # if more steps for agent x so check interrupt
+    if step < len(route[0]):
+        new_route = []
+        for h in range(0, step):
+            new_route.append(route[a][h])
+        start = (((node[0][0][0] + action[0], node[0][0][1] + action[1])), step)
+        goal = route[a][-1][0]
+        if step >= 1:
+            route[a] = astar(grid, start, goal, a)
+            for h in route[a]:
+                new_node = ((h[0]), new_route[-1][1] + 1)
+                new_route.append(new_node)
+            route[a] = new_route
+        else:
+            route[a] = astar(grid, start, goal, a)
+    else:
+        new_node = route[a][-1][0]
+        route[a].append(new_node)
+    #Run over all agents but agent 0
     for x in range(1, len(route)):
 
-        #if more steps for agent x so check interrupt
+        #if there are more steps for agent x so check interrupt
         if step < len(route[x]) and step < len(route[0]):
 
-            # If interrupt - update plan using A* for all agents
-            if tmp_node[0] == route[x][step][0] and k > 0:
-                agent_that_interrupt = x
-                print("agent: "+str(agent_that_interrupt)+" interrupted. attacker moved: "+str(node)+" step "+ str(step))
-                for a in range(0, len(route)-1):
-                    if a == 0:
-                        if step >= len(route[a]):
-                            route[a].append((tmp_node[0], step))
-                        else:
-                            route[a][step] = (tmp_node[0], step)
+            # !!!THIS LINE IS CHECKING THE INTERRUPTION!!!
+            if route[a][step][0] == route[x][step][0] and node[1] > 0:
 
-                    # send agent0 to A* (grid, start = next new step, goal, agent no.)
-                    if a is not agent_that_interrupt:
-                        start = route[a][step]
-                        goal = route[a][-1][0]
-                        if step >= 1:
-                            new_route = []
-                            for h in range(0, step):
-                                new_route.append(route[0][h])
-                            route[a] = astar(grid, start, goal, a)
-                            for h in route[0]:
-                                new_node = ((h[0]), new_route[-1][1] + 1)
-                                new_route.append(new_node)
-                            route[a] = new_route
-                        else:
-                            route[a] = astar(grid, start, goal, a)
-                    else:
-                        new_route = []
-                        for h in range(0, step):
-                            new_route.append(route[a][h])
-                        start = route[a][step - 1]
-                        goal = route[a][-1][0]
-                        if step >= 1:
-                            route[a] = astar(grid, start, goal, a)
-                            for h in route[a]:
-                                new_node = ((h[0]), new_route[-1][1] + 1)
-                                new_route.append(new_node)
-                            route[a] = new_route
-                        else:
-                            route[a] = astar(grid, start, goal, a)
-                k -= 1
+                #Insert stay action for the interrupt agent
+                route[x].insert(step, (route[x][step-1][0], step))
+                #Update steps no. for agent x
+                for y in range(step+1, len(route[x])):
+                    tmp_node2 = (route[x][y][0], route[x][y][1] + 1)
+                    route[x][y] = tmp_node2
 
-    # Set starting positions for all agents into a node
+    # Set positions for all agents into a node
     pos.append(tmp_node[0])
     for x in range(1, len(route)):
         if step < len(route[x]):
             pos.append(route[x][step][0])
         else:
             pos.append(route[x][-1][0])
-    tmp_node = (pos, k, step)
+    tmp_node = (pos, node[1]-1, step, node)
+
     return tmp_node
 
+solution = []
+solution = search_for_interrupt_plan()
+print(solution)
 
-search_for_interrupt_plan()
-print(route)
+for x in range(0, len(solution)):
+    new_total_g = new_total_g + len(solution[x])
+    start = solution[x][0][0]
+    goal = solution[x][-1][0]
+    colors = ['r', 'black', 'black', 'black', 'black']#'purple', 'b', 'c', 'm', 'y', 'k', 'w']
+    #if route:
+        #print('-'*20, 'Agent No. - ', x, '-'*20)
+        #print('Start point - ', start, '\nGoal point  - ', goal)
+        #print('Len of the route - ', len(route[x]))
+        #print('-'*56, '\n')
 
-for x in range(0, len(route)):
-
-    start = route[x][0][0]
-    goal = route[x][-1][0]
-
-    if route:
-        print('-'*20, 'Agent No. - ', x, '-'*20)
-        print('Start point - ', start, '\nGoal point  - ', goal)
-        print('Len of the route - ', len(route[x]))
-        print('-'*56, '\n')
-
+    colors = ['r', 'purple', 'b', 'c', 'm', 'y', 'k', 'w']
     ##############################################################################
     # plot the path
     ##############################################################################
@@ -294,12 +311,20 @@ for x in range(0, len(route)):
             y_coords.append(y1)
 
     # plot path
-    ax.scatter(start[1], start[0], marker="*", color="red", s=50)
-    ax.scatter(goal[1], goal[0], marker="*", color="purple", s=50)
-    ax.plot(y_coords, x_coords, color="blue")
+    ax.scatter(start[1], start[0], marker="*", color="black", s=50)
+    ax.scatter(goal[1], goal[0], marker="*", color="y", s=50)
+    if x <= len(colors):
+        ax.plot(y_coords, x_coords, color=colors[x], label=x)
+    else:
+        ax.plot(y_coords, x_coords, color=colors[len(colors)], label=x)
 
+    print(route[x])
+
+print('Original g_score is - ', org_total_g)
+print('New g_score is - ', new_total_g)
+
+minor_ticks = np.arange(0, 261, 1)
+ax.set_xticks(minor_ticks, minor=True)
+ax.set_yticks(minor_ticks, minor=True)
+ax.grid(which='both')
 plt.show()
-
-
-
-
